e794dc7fd7c5880d6f9c31d9fce256e0d3562748
Asal Nesar <assal.nobari@gmail.com>
2021-09-27 10:49:24 +0200

    Changed colors

--- src/components/ScoreDetails.js
+++ src/components/ScoreDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const COLORS = ["#7030a0", "#00b050", "#ffc000", "#0070c0"];
+const COLORS = ["#14B1B2", "#FF910B", "#04A2DF", "#F05E67"];
 
 export const ScoreRow = ({ name, value, color }) => {
   return (
